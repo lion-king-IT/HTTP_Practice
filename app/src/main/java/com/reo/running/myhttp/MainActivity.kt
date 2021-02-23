10f56d6e22130d91a4b42e76f08535b659584e25
github lion-king-IT <reo.runningg@gmail.com>
2021-02-23 10:30:50 +0900

    プラグイン追加

---
+++ app/src/main/java/com/reo/running/myhttp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.reo.running.myhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import com.squareup.okhttp.Dispatcher
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
